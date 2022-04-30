7a4b2197d64b16bd1bc2f4285921347b10c3f041
GeorgeandSavva <prokopev_savva@mail.ru>
2022-04-30 23:36:18 +0300

    scss and html coding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React from 'react';
+import logoSvg from './assets/img/ufo-2.png';
+
 
 
 function App() {
-  return (
-    <div >
-    Hello1
-    </div>
-  );
+    return (
+        <div className="page">
+            <header className="header">
+            <div className='header__inner'>
+                    <img className='header__logo' src={logoSvg} alt="ufo logo"></img>
+                    <div className='header__name'>LANIAKEA</div>
+                </div>
+            </header>
+
+            <aside className="sidebar">
+                <div className='sidebar__categories'>
+                </div>
+            </aside>
+
+            <main className="main">
+                <div className="container">
+                    Hello!
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
