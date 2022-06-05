a8175bd2270f48c6cbfefbef0f8557dbb48e95f0
GeorgeandSavva <prokopev_savva@mail.ru>
2022-06-05 21:53:08 +0300

    change pagination

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,6 @@ import { Galaxy } from '../pictures'
 import { filterSlice } from "../store/reducers/FilterSlice"
 import { postSlice } from "../store/reducers/PostSlice"
 
-
 const Header = () => {
     const dispatch = useAppDispatch()
     const {setCategory} = filterSlice.actions
@@ -22,52 +21,51 @@ const Header = () => {
     
     return(
         <header className="header">
-                <div className="header__wrapper">
-                    <div className="header__inner">
-                    <Link to="/" className="header__link">
-                        <img className="header__logo" src={String(Galaxy)} alt="ufo logo"></img>
-                        <div className="header__name">LANIAKEA</div>
-                    </Link>
-                    
-                        <nav className="nav">
-                            <ul className="nav__list">
-                                <li className="nav__item">
-                                <Link to="/" className='post__link'>
-                                    <div className="nav__link" onClick={() => onClickCategory()}>Главная</div>
-                                </Link>
-                                </li>
-                                <li className="nav__item">
-                                    <div className="nav__link">Обо мне</div>
-                                </li>
-                                <li className="nav__item">
-                                    <div className="nav__link">Отзывы</div>
-                                </li>
-                                <li className="nav__item">
-                                    <div className="nav__link">Реклама</div>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-
-                    <div className="toggleWrapper">
-                        <input type="checkbox" className="dn" id="dn" />
-                        <label htmlFor="dn" className="toggle">
-                            <span className="toggle__handler">
-                                <span className="crater crater--1"></span>
-                                <span className="crater crater--2"></span>
-                                <span className="crater crater--3"></span>
-                            </span>
-                            <span className="star star--1"></span>
-                            <span className="star star--2"></span>
-                            <span className="star star--3"></span>
-                            <span className="star star--4"></span>
-                            <span className="star star--5"></span>
-                            <span className="star star--6"></span>
-                        </label>
-                    </div>
-
+            <div className="header__wrapper">
+                <div className="header__inner">
+                <Link to="/" className="header__link">
+                    <img className="header__logo" src={String(Galaxy)} alt="ufo logo"></img>
+                    <div className="header__name">LANIAKEA</div>
+                </Link>
+                
+                    <nav className="nav">
+                        <ul className="nav__list">
+                            <li className="nav__item">
+                            <Link to="/" className='post__link'>
+                                <div className="nav__link" onClick={() => onClickCategory()}>Главная</div>
+                            </Link>
+                            </li>
+                            <li className="nav__item">
+                                <div className="nav__link">Обо мне</div>
+                            </li>
+                            <li className="nav__item">
+                                <div className="nav__link">Отзывы</div>
+                            </li>
+                            <li className="nav__item">
+                                <div className="nav__link">Реклама</div>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
-            </header>
+
+                <div className="toggleWrapper">
+                    <input type="checkbox" className="dn" id="dn" />
+                    <label htmlFor="dn" className="toggle">
+                        <span className="toggle__handler">
+                            <span className="crater crater--1"></span>
+                            <span className="crater crater--2"></span>
+                            <span className="crater crater--3"></span>
+                        </span>
+                        <span className="star star--1"></span>
+                        <span className="star star--2"></span>
+                        <span className="star star--3"></span>
+                        <span className="star star--4"></span>
+                        <span className="star star--5"></span>
+                        <span className="star star--6"></span>
+                    </label>
+                </div>
+            </div>
+        </header>
     )
 }
 
--- src/components/homeComponents/Pagination.tsx
+++ src/components/homeComponents/Pagination.tsx
@@ -32,26 +32,25 @@ const Pagination: React.FC<IPagination> = ({ category }) => {
     let rightPortionPageNumber = portionNumber * portionSize
 
     return (
-            <ul className="pagination">
-                <li className="pagination__item">
-                    <a className={portionNumber > 1 ? "pagination__link" : "pagination__link pagination__link--button"}
-                    onClick={portionNumber > 1 ? () => {onPortionChanged(portionNumber - 1)} : () => {}} >&lt;</a>
-                </li>
+        <ul className="pagination">
+            <li className="pagination__item">
+                <a className={portionNumber > 1 ? "pagination__link" : "pagination__link pagination__link--button"}
+                onClick={portionNumber > 1 ? () => {onPortionChanged(portionNumber - 1)} : () => {}} >&lt;</a>
+            </li>
 
-                {pages
-                    .filter(p => p >= leftPortionPageNumber && p<=rightPortionPageNumber)
-                    .map((p, index) => {
-                        return (
-                            <li className="pagination__item" key={index} onClick={() => onPageChanged(p)}>
-                                <a className={currentPage === p ? "pagination__link  pagination__link--active" : "pagination__link"} >{p}</a>
-                            </li>)
-                })}
-                
-                <li className="pagination__item">
-                    <a className={portionCount > portionNumber ? "pagination__link" : "pagination__link pagination__link--button"}
-                    onClick={portionCount > portionNumber ? () => {onPortionChanged(portionNumber + 1)} : () => {}}>&gt;</a>
-                </li>
-            </ul>
+            {pages
+                .filter(p => p >= leftPortionPageNumber && p<=rightPortionPageNumber)
+                .map((p, index) => (
+                    <li className="pagination__item" key={index} onClick={() => onPageChanged(p)}>
+                        <a className={currentPage === p ? "pagination__link  pagination__link--active" : "pagination__link"} >{p}</a>
+                    </li>
+                ))}
+            
+            <li className="pagination__item">
+                <a className={portionCount > portionNumber ? "pagination__link" : "pagination__link pagination__link--button"}
+                onClick={portionCount > portionNumber ? () => {onPortionChanged(portionNumber + 1)} : () => {}}>&gt;</a>
+            </li>
+        </ul>
     )
 }
 
